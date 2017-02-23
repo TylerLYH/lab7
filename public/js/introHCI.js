33aa8f6e714054110ad6260ef51e5e8905508d60
Tyler Li <Tyler Li>
2017-02-23 10:35:03 -0800

    GAtracking

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,10 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".project").click(function () {
+
+	ga("send", "event", 'like', 'click');
+	
+	});
 }
+
